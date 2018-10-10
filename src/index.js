f9daa6e9ca4b6441fbb02c34e677e6c54c4138eb
Xuemuyang <myoungxue@gmail.com>
2018-10-11 00:26:44 +0800

    路由初步完成

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import App from './App'
 import { counter } from './index.redux'
 
@@ -12,9 +13,34 @@ const store = createStore(counter, compose(
   reduxDevtools
 ))
 
+function One() {
+  return <h1>一</h1>
+}
+
+function Two() {
+  return <h1>二</h1>
+}
+
 ReactDom.render(
   (<Provider store={store}>
-      <App/>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+            <li>
+              <Link to='/one'>一</Link>
+            </li>
+            <li>
+              <Link to='/two'>二</Link>
+            </li>
+          </ul>
+          <Route path='/' exact component={App}></Route>
+          <Route path='/one' component={One}></Route>
+          <Route path='/two' component={Two}></Route>
+        </div>
+      </BrowserRouter>
     </Provider>
   ),
   document.getElementById('root')
