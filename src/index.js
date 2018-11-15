f330e4800d31b625eca549047ec4b5ebed3c738e
Xuemuyang <myoungxue@gmail.com>
2018-11-15 21:54:02 +0800

    项目初始化

--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Redirect,Switch } from 'react-router-dom'
 
-import Login from './container/login/login'
-import Register from './container/register/register'
-import AuthRoute from './component/authroute/authroute'
 import reducers from './reducer'
 import './config'
-import './index.css'
 
 const store = createStore(reducers, compose(
 	applyMiddleware(thunk),
