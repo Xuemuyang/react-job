70ea1041c0373722a8d6199b55ba1043a690d0de
Xuemuyang <myoungxue@gmail.com>
2019-01-08 09:53:14 +0800

    feat: 文档补充

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -5,7 +5,17 @@ import { List, InputItem, WingBlank, WhiteSpace, Button } from "antd-mobile";
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: '',
+      pwd: ''
+    }
     this.register = this.register.bind(this);
+  }
+
+  handleChange(key, val) {
+    this.setState({
+      [key]: val
+    });
   }
 
   register() {
@@ -20,9 +30,9 @@ class Login extends React.Component {
         <WingBlank>
           <List>
             <WhiteSpace />
-            <InputItem>用户名</InputItem>
+            <InputItem onChange={v => this.handleChange("user", v)}>用户名</InputItem>
             <WhiteSpace />
-            <InputItem>密码</InputItem>
+            <InputItem onChange={v => this.handleChange("pwd", v)}>密码</InputItem>
           </List>
           <WhiteSpace />
           <Button type="primary">登录</Button>
